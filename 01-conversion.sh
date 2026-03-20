af0a6e6a01b6bb3c376445cbf365a18b40d6555c
laxmiplp <laxmiprasanna.plp@gmail.com>
2026-03-20 06:50:19 +0530

    initial commit

---
+++ 01-conversion.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Suresh:: Hi Ramesh! How are you?"
+echo "Ramesh:: Hi Suresh! I'm doing good! How about you?"
+echo "Suresh:: I'm too good! Had your dinner?"
+echo "Ramesh:: No Suresh, I'm about to had now"
+echo "Suresh:: Oh that's great! come Ramesh let's had together"
+echo "Ramesh:: My goodness, Thankyou Suresh for offering the company!"
+echo "Suresh:: 'It's my pleasure Ramesh!"
