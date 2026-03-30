d57f9e58379ba68502fce5204096ba44e02f7def
Sahasra-DevSecops <uicloudops@gmail.com>
2026-03-31 00:04:37 +0530

    logs modified

--- 012-logs.sh
+++ 012-logs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOGS_FOLDER= "/var/log/shell-scripting"
-LOGS_FILE= "/var/log/shell-scripting/$0.log"
+LOGS_FOLDER="/var/log/shell-scripting"
+LOGS_FILE="/var/log/shell-scripting/$0.log"
+
 mkdir -p $LOGS_FOLDER
+
 validate(){
 if [  $1 -ne 0 ]; then
 echo "$2 installation failure......"
@@ -12,9 +14,12 @@ else
 echo "$2 installation success......"
 fi
 }
- dnf install nginx -y &>> $LOGS_FILE
+
+dnf install nginx -y &>> $LOGS_FILE
 validate $? "Installing nginx......"
+
 dnf install mysql -y &>> $LOGS_FILE
-validate $? "Installingmysql.........."
+validate $? "Installing mysql.........."
+
 dnf install nodejs -y &>> $LOGS_FILE
 validate $? "Installing nodejs......."
